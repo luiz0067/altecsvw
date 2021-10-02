8dc879bc1346aaca92179b6a179c6f455a75a78a
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-02 17:45:19 -0300

    Add files via upload

---
+++ adm - Cópia/adm/menu.php
@@ -0,0 +1,11 @@
+<center>
+<a href="./cadastro_usuario.php">Cadastro de usuario</a><br>
+<a href="./cadastro_categoria.php">Cadastro de categoria</a><br>
+<a href="./cadastro_patrocinio.php">Cadastro patrocinio</a><br>
+<a href="./cadastro_home.php">Cadastro home</a><br>
+<a href="./cadastro_esporte.php">Cadastro esportes</a><br>
+<a href="./cadastro_eventos.php">Cadastro eventos</a><br>
+<a href="./cadastro_projetos.php">Cadastro projetos</a><br>
+<a href="./cadastro_quem_somos.php">Cadastro quem somos</a><br>
+<a href="./cadastro_download.php">Cadastro download</a><br>
+</center>
